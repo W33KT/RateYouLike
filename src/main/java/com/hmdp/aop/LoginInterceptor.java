7accb5c94a261e28d16427e421816fa67acceae9
tankaiwen <weektabc@gmail.com>
2025-09-05 20:07:55 +0200

    Login: verify login info for other api

---
+++ src/main/java/com/hmdp/aop/LoginInterceptor.java
@@ -0,0 +1,37 @@
+package com.hmdp.aop;
+
+import com.hmdp.constants.ResponseCode;
+import com.hmdp.constants.SystemConstants;
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import com.hmdp.utils.UserHolder;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.Objects;
+
+/**
+ * @author tankaiwen
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+
+        Object user = session.getAttribute(SystemConstants.SESSION_USER_KEY);
+        if (Objects.isNull( user)) {
+            response.setStatus(ResponseCode.NOT_LOGIN);
+            return false;
+        }
+
+        UserHolder.saveUser(UserDTO.convertFromUser((User) user));
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
--- src/main/java/com/hmdp/dto/UserDTO.java
+++ src/main/java/com/hmdp/dto/UserDTO.java
@@ -1,10 +1,25 @@
 package com.hmdp.dto;
 
+import com.hmdp.entity.User;
 import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.util.Objects;
 
 @Data
+@Accessors(chain = true)
 public class UserDTO {
     private Long id;
     private String nickName;
     private String icon;
+
+    public static UserDTO convertFromUser(User user) {
+        if (Objects.isNull( user)) {
+            return null;
+        }
+        return new UserDTO()
+                .setId(user.getId())
+                .setNickName(user.getNickName())
+                .setIcon(user.getIcon());
+    }
 }
